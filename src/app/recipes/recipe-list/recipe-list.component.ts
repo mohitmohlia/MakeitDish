e4164e096a2fff78b429f094721141af181953f7
mohitmohlia <mohitmohlia96@gmail.com>
2020-05-09 23:28:25 +0530

    showing a recipe on page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+recipes:Recipe[]=[
+  new Recipe('A test recipe', 'this is simple a test','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chicken_freekeh_chopped_salad_with_leftover_salsa_verde.jpg');
+];
   constructor() { }
 
   ngOnInit() {
